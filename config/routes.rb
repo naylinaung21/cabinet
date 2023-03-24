0b6d202fc5f6b3eaa156e2c98fff0a3576790f95
Aikekwan <aikekwan@gmail.com>
2023-03-24 22:40:06 +0630

    cabinet crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  resources :docs
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+
+  # Defines the root path after authentication
+  authenticated :user do
+    root "docs#index", as: 'authenticated_root'
+  end
+  # Defines the root path before authentication
+  root 'welcome#index'
 end
